50da8d8accbf8f5b5ed082fd78c663cd3983e43b
saravananbass12-tech <saravananbass12@gmail.com>
2026-06-01 08:42:56 +0530

    Add files via upload

---
+++ saravanan 1.py
@@ -0,0 +1,102 @@
+# Task 11
+
+balance=15000
+deposite=5000
+balance+=deposite
+print("update balance ;",balance)
+
+# Task 12
+
+student=60
+leave=5
+student-=leave
+print("remaining student ;",student)
+
+# Task 13
+
+salary=8000
+triple=3
+salary*=triple
+print("salary ;",salary)
+
+# Task 14
+
+price=950
+Gst=50 
+price+=Gst
+print("final price ;",price)
+
+# Task 15
+
+score=120
+runs=35
+score+=runs
+print("total score ;",score)
+
+
+# Task 16
+
+battery=100
+reduce=40
+battery-=reduce
+print("remaining battery ;",battery)
+
+
+# Task 17
+
+cows=20
+comes=2
+cows*=comes
+print("total cows ;",cows)
+
+
+# Task 18
+
+pens=75
+using=20
+pens-=using
+print("remaining pens ;",pens)
+
+
+# Task 19
+
+mark=72
+increas=8
+mark+=increas
+print("total mark ;",mark)
+
+
+# Task 20
+
+number=7
+multiplied=10
+number*=multiplied
+print("numbers ;",number)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
